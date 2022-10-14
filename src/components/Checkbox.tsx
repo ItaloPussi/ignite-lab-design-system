06ee915818ec9ebb88ac584a04ab7f47fd5b6669
ItaloPussi <italopussi@gmail.com>
2022-10-13 22:30:24 -0300

    Ignite Lab - Design System: App Page

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,12 +1,13 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps){
+export function Checkbox({id}: CheckboxProps){
     return (
         <CheckboxPrimitive.Root 
             className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+            id={id}
         >
             <CheckboxPrimitive.Indicator asChild>
                 <Check size={20} weight="bold" className='text-cyan-500' />
